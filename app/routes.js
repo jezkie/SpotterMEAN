12969ab2da95c8426ba751b1daf396701d93afbd
jezkie <jezkie00secondary@gmail.com>
2015-10-29 01:39:33 +0800

    Get by id and add route

--- app/routes.js
+++ app/routes.js
@@ -8,17 +8,28 @@ module.exports = function(app) {
 
   // frontend routes =========================================================
   // route to handle all angular requests
+
+
+  app.get('/api/exercises/:id', function(req, res){
+    Exercise.findById(req.params.id, function(err, doc) {
+      if (err)
+        res.send(err);
+
+      res.json(doc);
+    });
+  })
+
   app.get('/api/exercises', function(req, res) {
-      Exercise.find(/*{'name': 'Squat'},*/ function(err, doc) {
+      Exercise.find(function(err, doc) {
         if (err)
           res.send(err);
 
         res.json(doc);
       });
 
-    });
+    })
 
-  app.post('/api/exercises', function(req, res){
+  app.post('/api/exercises/add', function(req, res){
     var exercise;
     console.log(req.body);
 
@@ -37,6 +48,26 @@ module.exports = function(app) {
     });
 
     return res.send(exercise);
+  })
+
+  app.post('/api/exercises/edit', function(req, res){
+
+    console.log(req.body);
+
+    Exercise.findById(req.body._id, function(err, exercise){
+      exercise.name = req.body.name;
+      exercise.rest = req.body.rest;
+
+      exercise.save(function(err){
+        if (!err){
+          console.log('created');
+        } else {
+          console.log(err);
+        }
+      });
+
+      return res.send(exercise);
+    });
 
   })
 };
